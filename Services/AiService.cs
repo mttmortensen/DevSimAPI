8419a1d789196cb70424f701311d98636bc527f9
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-22 16:54:54 -0600

    constructor

--- Services/AiService.cs
+++ Services/AiService.cs
@@ -1,6 +1,17 @@
-﻿namespace DevSimAPI.Services
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.Text;
+
+namespace DevSimAPI.Services
 {
     public class AiService
     {
+        private readonly OpenAIClient _client;
+
+        public AiService(IConfiguration config)
+        {
+            var apiKey = config["OpenAI:ApiKey"];
+            _client = new OpenAIClient(apiKey);
+        }
     }
 }
