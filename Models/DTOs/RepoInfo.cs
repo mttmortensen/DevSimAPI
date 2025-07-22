5b1e4c5a095a802dae93523a45fc48a456e6b925
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-22 16:52:18 -0600

    simple dto for ai service to use to generate tasks

---
+++ Models/DTOs/RepoInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace DevSimAPI.Models.DTOs
+{
+    /// <summary>
+    /// DTO Model used for AI_Service to generate issues and inject back into GH Issues
+    /// </summary>
+    public class RepoInfo
+    {
+        public string RepoName { get; set; }
+        public string Description { get; set; }
+    }
+}
